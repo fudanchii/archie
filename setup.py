2cbc87b78851ca9e8a3637e88d192944dc0c5e96
Nurahmadie <nurahmadie@gmail.com>
2014-01-15 03:36:40 +0700

    Add installation requirements to setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ from distutils.core import setup
 from archie import APPNAME, VERSION
 
 setup(
-    name         = APPNAME,
-    version      = VERSION,
-    description  = 'Symlink management for configuration (rc) file.',
-    author       = 'Nurahmadie',
-    author_email = 'nurahmadie@gmail.com',
-    url          = 'http://bitbucket.org/fudanchii/archie',
-    packages     = ['archie', 'archie.handlers'],
-    scripts      = ['arc'],
-    classifiers=[
+    name             = APPNAME,
+    version          = VERSION,
+    description      = 'Symlink management for configuration (rc) file.',
+    author           = 'Nurahmadie',
+    author_email     = 'nurahmadie@gmail.com',
+    url              = 'http://bitbucket.org/fudanchii/archie',
+    packages         = ['archie', 'archie.handlers'],
+    scripts          = ['arc'],
+    install_requires = ['docopt'],
+    classifiers      = [
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: System Administrators',
